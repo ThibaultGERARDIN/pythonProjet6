169c211fe3b93f7d615c5633a111f0c59971a9d5
Thibault G <thibault.gerardin@gmail.com>
2024-11-05 17:16:11 +0100

    début des media queries

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ import { bestMovie } from "./modules/bestmovie.js";
 import { otherMovies } from "./modules/othermovies.js";
 import { showModal } from "./modules/modale.js";
 
+
 bestMovie()
 createVignette("Fantasy")
 createVignette("Sci-Fi")
